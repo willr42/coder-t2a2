2d05d4452f44e967f9a437d9bd31c7c539b14e62
Will Robertson <williamdrobertson42@gmail.com>
2023-03-02 20:40:29 +1100

    feat: initial Flask skeleton

---
+++ src/main.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+ma = Marshmallow()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("config.app_config")
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    from commands import db_commands
+
+    app.register_blueprint(db_commands)
+
+    from controllers import registerable_controllers
+
+    for controller in registerable_controllers:
+        app.register_blueprint(controller)
+
+    return app
