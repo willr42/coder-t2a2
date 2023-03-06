b30ec85f31f1ccee2d4d637b41abf45980bcb698
Will Robertson <williamdrobertson42@gmail.com>
2023-03-06 13:05:46 +1100

    fix: remove pguser, change dir structure

--- src/setup.sh
+++ src/setup.sh
@@ -8,6 +8,8 @@ silent(){
 	"$@" > /dev/null 2>&1
 }
 
+# move to directory of the script
+cd -P -- "$(dirname -- "$0")"
 # move to temporary directory, otherwise we get error output when we switch to postgres user
 silent pushd /tmp || echo "Something went wrong changing to the temporary dir"
 
@@ -17,14 +19,16 @@ if ! [ -x "$(command -v psql)" ]; then
 	echo -e "Please consult https://www.postgresql.org/ for instructions.${NC}"
 	exit 1
 fi
+
 echo "Creating database and user..."
-sudo su postgres -c "psql -U postgres -tc \"SELECT 1 FROM pg_user WHERE usename = 'plantadmin'\" | grep -q 1 || psql -U postgres -c \"CREATE USER plantadmin\""
-sudo su postgres -c "echo \"SELECT 'CREATE DATABASE plantapi' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'plantapi')\gexec\" | psql"
+psql -c "SELECT 1 FROM pg_user WHERE usename = 'plantadmin'" | grep -q 1 || psql -c "CREATE USER plantadmin"
+echo "SELECT 'CREATE DATABASE plantapi' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'plantapi')\gexec" | psql
 echo "User and database created successfully."
 silent popd
 
+read -p "Postgres port (leave blank for default): " POSTGRES_PORT
 echo "Generating .env file..."
 cat << EOF > .env
-DATABASE_URL="postgresql+psycopg2://plantadmin:plants@${HOSTNAME}/plantapi"
+DATABASE_URL="postgresql+psycopg2://plantadmin:plants@localhost:${POSTGRES_PORT:-5432}/plantapi"
 SECRET_KEY="example_secret_key"
 EOF
