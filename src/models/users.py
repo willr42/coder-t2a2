7de8603208f4530727cddd2377175dfc84905410
Will Robertson <williamdrobertson42@gmail.com>
2023-03-13 16:21:12 +1100

    feat: add password column

--- src/models/users.py
+++ src/models/users.py
@@ -7,6 +7,7 @@ class User(db.Model):
     user_id = db.Column(db.Integer, nullable=False, primary_key=True)
     full_name = db.Column(db.String(), nullable=False)
     email = db.Column(db.String(), nullable=False, unique=True)
+    password = db.Column(db.String(), nullable=False)
     expert = db.Column(db.Boolean, nullable=False, default=False)
 
     garden = db.relationship("Garden", back_populates="user")
