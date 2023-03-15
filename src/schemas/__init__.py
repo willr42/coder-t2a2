b1c75ab300e5bba5b860c21c111eb497788f3743
Will Robertson <williamdrobertson42@gmail.com>
2023-03-15 20:03:00 +1100

    feat: barrel imports for schemas

---
+++ src/schemas/__init__.py
@@ -0,0 +1,4 @@
+from .garden_plant_schema import garden_plant_schema, garden_plants_schema
+from .garden_schema import garden_schema, gardens_schema
+from .plant_schema import plant_schema, plants_schema
+from .user_schema import user_schema
