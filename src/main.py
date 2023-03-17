b317926418be8316eded250beb0bbcbf05f54798
Will Robertson <williamdrobertson42@gmail.com>
2023-03-17 18:21:24 +1100

    fix: sort imports

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 ma = Marshmallow()
