aa36995c1fe9d961c05c3d24d4e9a39462e493ac
Will Robertson <williamdrobertson42@gmail.com>
2023-03-18 10:48:30 +1100

    feat: add garden_plant_controller to routes

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,5 +1,11 @@
 from .auth_controller import auth_blueprint
 from .garden_controller import garden_blueprint
+from .garden_plant_controller import garden_plant_blueprint
 from .plant_controller import plant_blueprint
 
-registerable_controllers = [auth_blueprint, plant_blueprint, garden_blueprint]
+registerable_controllers = [
+    auth_blueprint,
+    plant_blueprint,
+    garden_blueprint,
+    garden_plant_blueprint,
+]
